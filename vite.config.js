e1380ee036bf8c64598b45053f22b920e93e2663
Chris Minnick <chris@minnick.com>
2025-08-06 15:40:36 -0700

    configuring ci/cd for pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: '/pet-vue/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/pet-vue/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
