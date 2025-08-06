80f5842a6aac2c64fd8f6adfc028da71e45f41b2
Chris Minnick <chris@minnick.com>
2025-08-06 16:06:21 -0700

    modified config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/pet-vue/' : '/',
   base: '/pet-vue/',
