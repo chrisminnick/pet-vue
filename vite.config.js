19aa14e6915991d6c6cb226c679c8eb1a435e599
Chris Minnick <chris@minnick.com>
2025-08-06 16:12:47 -0700

    added publicpath

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/pet-vue/' : '/',
+}
+export default defineConfig({
   base: '/pet-vue/',
   plugins: [vue(), vueDevTools()],
   resolve: {
